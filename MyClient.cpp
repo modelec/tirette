aa75c0c8b0eb7a763697b63db0dff02d1d1889c5
ackimixs <ackimixs@protonmail.com>
2024-03-27 17:26:53 +0100

    patch pointer

--- MyClient.cpp
+++ MyClient.cpp
@@ -12,7 +12,7 @@ void MyClient::handleMessage(const std::string& message)
     // if get tirette => send tiretteState
     if (action == "tirette") {
         if (split(message, ";")[2] == "get tirette_state") {
-            const std::string toSend = "tirette;start;set tirette_state;" + std::to_string(*tiretteState);
+            const std::string toSend = "tirette;start;set tirette_state;" + std::to_string(tiretteState);
             this->sendMessage(toSend.c_str());
         }
     } else if (action == "ping")
